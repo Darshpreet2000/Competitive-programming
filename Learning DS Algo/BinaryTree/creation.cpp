4899906f36f29402fbf45ac9a8c3d9e7c523dfb5
Darshpreet Singh <dsingh200021@gmail.com>
2020-12-22 12:27:13 +0530

    Update creation.cpp

--- Learning DS Algo/BinaryTree/creation.cpp
+++ Learning DS Algo/BinaryTree/creation.cpp
@@ -8,7 +8,7 @@ class node{
     node(int d){
        data=d;
        left=NULL;
-       right=NULL;np->
+       right=NULL;
     }
 };
 node *buildTree(){
@@ -20,7 +20,7 @@ node *buildTree(){
     np->right=buildTree();
     return np;
 }
-void print(node *root){
+void preorder(node *root){
     if(root==NULL) return;
    cout<<root->data<<" ";
 
@@ -29,10 +29,31 @@ void print(node *root){
      print(root->right);
     return;
 }
+void inorder(node *root){
+    if(root==NULL) return;
+
+   print(root->left);
+
+   cout<<root->data<<" ";
+     print(root->right);
+    return;
+}
+void postorder(node *root){
+    if(root==NULL) return;
+
+   print(root->left);
+
+     print(root->right);
+   cout<<root->data<<" ";
+
+    return;
+}
 int main() {
      node *root=buildTree();
 
-     print(root);
+     preorder(root);
+     inorder(root);
+     postorder(root);
 
 }
 
